5d6184a79412d461571a61a983bc2b62af37d4f6
Nick White <nickswhite89@gmail.com>
2014-01-15 21:52:11 +0000

    Creates the Distance convertor function
    - Split into 2 functions to seperate the For Loops

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-js.geo-distance
-===============
+#js.geo-distance
+
+####About JS Geo Distance
+
+This plugin was build based on Chris Veness' JavaScript plugin found www.movable-type.co.uk/scripts/latlong.html
+
+Using
+-----
+
+```js
+var to = [
+    {
+        lat: -22.853840,
+        lng: 19.882812
+    },
+    {
+        lat: 44.514772,
+        lng: 20.058594
+    }
+];
+var from = {
+    lat: 58.114637,
+    lng: -4.375000
+};
+
+
+geoDistance.run(to, from, 'feet', 2);
+```
---
+++ src/distance.js
@@ -0,0 +1,129 @@
+/**
+ * @author  Nick White <nickswhite89@gmail.com>
+ * @description Returns the distance between Longitude and Latitude geographical
+ *              distances
+ * @version 1.0.0
+ * @copyright Nick White
+ *
+ * Free to use under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Based Latitude/longitude spherical geodesy formulae & scripts
+ * Copyright 2011 Chris Veness
+ * Project Home: www.movable-type.co.uk/scripts/latlong.html
+ *
+ */
+
+(function () {
+
+    if (typeof(Number.prototype.toRad) === 'undefined') {
+        /**
+         * Converts numeric degrees to radians
+         * @return {number} Returns the radian of the number
+         */
+        Number.prototype.toRad = function () {
+            return this * Math.PI / 180;
+        };
+    }
+
+    /**
+     * GeoDistance
+     * @namespace  geoDistance
+     */
+    window.geoDistance = {
+        /**
+         * Function to retrieve the difference in a human readable length such
+         * as km between two difference longitude and latitude points
+         * @function init
+         * @memberOf geoDistance
+         * @param  {object} from    [Contains the main starting lng/lat point]
+         * @param  {number} from.lat [Latitude]
+         * @param  {number} from.lng [Longitutde]
+         * @param  {array<object>}  to [Can contain multiple end lng/lat points]
+         * @param  {number} to.lat [Latitude]
+         * @param  {number} to.lng [Longitude]
+         * @param  {string} [length=km]   [The human readable length such as KM]
+         * @param  {int} [decimals=2] [To which amount of decimals to return]
+         * @return {Object}          [Returns the to object with the distance]
+         */
+        init : function (from, to, length, decimals) {
+            decimals = decimals || 2;
+            length = length || 'km';
+
+            var earthRadius = this.distances[length],
+                fromLng = parseFloat(from.lng),
+                fromLat = parseFloat(from.lat);
+
+            fromLat = fromLat.toRad();
+
+            for (var i = to.length - 1; i >= 0; i--) {
+                var lngLat = to[i],
+                    toLat = parseFloat(lngLat.lat),
+                    toLng = parseFloat(lngLat.lng),
+                    dLat = (fromLat - toLat).toRad(),
+                    dLng = (fromLng - toLng).toRad(),
+                    data = {
+                        earthRadius: earthRadius,
+                        decimals: decimals,
+                        dLat: dLat,
+                        dLng: dLng,
+                        toLng: toLng,
+                        fromLat: fromLat
+                    };
+
+                data['toLat'] = toLat.toRad();
+
+                to[i]['distance'] = this.retrieveDistance(data);
+            }
+
+            return to;
+        },
+        /**
+         * Function which calculates a single distance between two points
+         * @function retrieveDistance
+         * @memberOf geoDistance
+         * @param  {object} data [Created by the run function]
+         * @param  {number} data.earthRadius [Contains the radius]
+         * @param  {number} data.decimals [Contains the amount of decimals]
+         * @param  {number} data.dLat [From minus to Lat radian]
+         * @param  {number} data.dLng [From minus to Lng radian]
+         * @param  {number} data.toLat [To Latitude]
+         * @param  {number} data.toLng [To Longitude]
+         * @param  {number} data.fromLat [From Latitude radian]
+         * @return {number}      [Returns the caculated distance]
+         */
+        retrieveDistance : function (data) {
+            var earthRadius = data.earthRadius,
+                decimals = data.decimals,
+                dLat = data.dLat,
+                dLng = data.dLng,
+                toLat = data.toLat,
+                toLng = data.toLng,
+                fromLat = data.fromLat,
+
+                a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                    Math.sin(dLng / 2) * Math.sin(dLng / 2) * Math.cos(toLat) *
+                    Math.cos(fromLat),
+                b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)),
+                c = earthRadius * b,
+                d = (Math.round(c * Math.pow (10, decimals)) /
+                    Math.pow(10, decimals)) * 1000;
+
+            return d;
+        },
+        /**
+         * Contains the radius of earth based on different distances such as
+         * miles or kilometers (km)
+         * @memberOf geoDistance
+         * @type {Object}
+         */
+        distances : {
+            'km': 6371,
+            'miles': 3959,
+            'metres': 6371000,
+            'yards': 6967410,
+            'feet': 20902231
+        }
+    };
+
+})();
