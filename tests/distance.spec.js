5adbe9439e50dba92605e9eb0fc90af4e4e5b3b9
Nick White <nickswhite89@gmail.com>
2014-08-24 11:04:16 +0100

    Adds further tests for each option

--- tests/distance.spec.js
+++ tests/distance.spec.js
@@ -104,4 +104,73 @@ describe('Get distance', function () {
 
     });
 
+    it('Should return the distance', function () {
+        var data = {
+                'earthRadius': 6967410,
+                'decimals': 2,
+                'dLat': -0.7592266063998,
+                'dLng': -0.4264466633955314,
+                'toLng': 20.058594,
+                'fromLat': 1.0142917592513199,
+                'toLat': 0.7769293371745812
+            },
+            distance = geoDist.retrieveDistance(data);
+
+        expect(distance).toBe(5623014900);
+    });
+
+    describe('Init function retrieves the distances', function () {
+        it('Uses geoDistance function sort and retrieve', function () {
+            spyOn(geoDist, 'distanceSort');
+            spyOn(geoDist, 'retrieveDistance');
+
+            geoDist.init(from, to, 'asc', 'yards', 2);
+
+            expect(geoDist.distanceSort).toHaveBeenCalled();
+            expect(geoDist.retrieveDistance).toHaveBeenCalled();
+        });
+
+        it('Doesn\'t need decimals or length set', function () {
+            var result = geoDist.init(from, to);
+
+            expect(result[0].distance).toBe(3265390);
+            expect(result[1].distance).toBe(5141690);
+        });
+
+        it('Takes a lng and lat using yards', function () {
+            var resultYards = geoDist.init(from, to, 'asc', 'yards', 2);
+
+            expect(resultYards[0].distance).toBe(3571078080);
+            expect(resultYards[1].distance).toBe(5623014900);
+        });
+
+        it('Takes a lng and lat using km', function () {
+            var resultKm = geoDist.init(from, to, 'asc', 'km', 2);
+
+            expect(resultKm[0].distance).toBe(3265390);
+            expect(resultKm[1].distance).toBe(5141690);
+        });
+
+        it('Takes a lng and lat using miles', function () {
+            var resultMiles = geoDist.init(from, to, 'asc', 'miles', 2);
+
+            expect(resultMiles[0].distance).toBe(2029150);
+            expect(resultMiles[1].distance).toBe(3195090);
+        });
+
+        it('Takes a lng and lat using metres', function () {
+            var resultMetres = geoDist.init(from, to, 'asc', 'metres', 2);
+
+            expect(resultMetres[0].distance).toBe(3265393950);
+            expect(resultMetres[1].distance).toBe(5141685060);
+        });
+
+        it('Takes a lng and lat using feet', function () {
+            var resultFeet = geoDist.init(from, to, 'asc', 'feet', 2);
+
+            expect(resultFeet[0].distance).toBe(10713234760);
+            expect(resultFeet[1].distance).toBe(16869045500);
+        });
+    });
+
 });
